cc83d19f804a51ff70b46c61daf8c889448e0564
tejasghlade <tejasghlade@gmail.com>
2024-01-14 16:14:12 +0530

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server"
 
+
 import { RegisterSchema } from "@/schemas";
 import { revalidatePath, revalidateTag } from "next/cache";
 import * as z from "zod"
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values : z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -11,7 +15,34 @@ export const register = async (values : z.infer<typeof RegisterSchema>) => {
         return {error : "Invalid fields !"}
     }
 
-    return {success : "Email sent!"}
+    const { email , password , name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    // Todo :: one method 
+    // const existingUser = await db.user.findUnique({
+    //     where : {
+    //         email,
+    //     }
+    // });
+
+    // Todo :: other method
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error : "Email already in use!"}
+    }
+
+    await db.user.create({
+        data : {
+            name , 
+            email , 
+            password : hashedPassword,
+        }
+    });
+
+    // Todo :: Send verification token email
+
+    return {success : "User Created"}
 
     // revalidatePath() next caching
     // revalidateTag()
